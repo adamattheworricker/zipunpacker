e41c8313a7399a5f7c95ceeb540fefbfcc65e737
Adam Worricker <40427309+adamattheworricker@users.noreply.github.com>
2024-09-05 11:13:35 -0400

    added optional removal of zip files on completion after unpacking

--- zipunpacker.ps1
+++ zipunpacker.ps1
@@ -11,3 +11,13 @@ foreach ($zip in $zips)
     {
        Expand-Archive $zip -Force 
     }
+
+Write-Host "Files unzipped, could you like to delete the zip files? (Y/N)"
+$response = Read-Host
+
+$response = $response.ToUpper()
+
+if ($response -eq "Y") {
+    Remove-Item "*.zip"
+}
+
